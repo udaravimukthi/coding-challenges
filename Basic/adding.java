504dc08aed44498bccb51bac09e13eb8e1434bf4
Udara Vimukthi <uvlakshan@gmail.com>
2021-08-17 21:01:37 +0530

    Added adding java class

---
+++ Basic/adding.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class adding{
+    public static void main (String args[]){
+    Scanner scan = new Scanner(System.in);
+    int x,y, z;
+    System.out.println("Enter first Number");
+    x = scan.nextInt() ;
+    System.out.println("Enter second Number");
+    y = scan.nextInt() ;
+    z = x+y;
+    System.out.println("total value of adding =" +z);
+}
+
+
+}
