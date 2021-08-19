3e22ab38fa58c2ada76d1495b958e7f4bb4670a4
Udara Vimukthi <uvlakshan@gmail.com>
2021-08-19 23:34:06 +0530

    Added new code for argument parsing

---
+++ Basic/arguments/arguments.java
@@ -0,0 +1,18 @@
+
+public class arguments{
+
+    public static void main(String args[]){
+    
+        int a[] = {2,4,5,5,3};
+        System.out.println(print(a)); 
+        
+       // System.out.println(a);
+    }
+
+    public static int print(int...x){
+        int total = 0;
+        for(int y:x)
+           total += y;
+           return total;
+    }
+}
