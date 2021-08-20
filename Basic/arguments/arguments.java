cabe7d6f2c466930d6cc4b1358e8f9d598ce5ef9
Udara Vimukthi <uvlakshan@gmail.com>
2021-08-20 23:41:08 +0530

    Added change in the file

--- Basic/arguments/arguments.java
+++ Basic/arguments/arguments.java
@@ -1,18 +1,18 @@
 
-public class arguments{
+// public class arguments{
 
-    public static void main(String args[]){
+//     public static void main(String args[]){
     
-        int a[] = {2,4,5,5,3};
-        System.out.println(print(a)); 
+//         int a[] = {2,4,5,5,3};
+//         System.out.println(print(a)); 
         
-       // System.out.println(a);
-    }
+//        // System.out.println(a);
+//     }
 
-    public static int print(int...x){
-        int total = 0;
-        for(int y:x)
-           total += y;
-           return total;
-    }
-}
+//     public static int print(int...x){
+//         int total = 0;
+//         for(int y:x)
+//            total += y;
+//            return total;
+//     }
+// }
