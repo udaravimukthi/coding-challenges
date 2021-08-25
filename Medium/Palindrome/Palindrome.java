79a8d4533554d78bc0568b099700cb735be0e016
Udara Vimukthi <uvlakshan@gmail.com>
2021-08-25 19:24:01 +0530

    Added to find a whether it is prime number

---
+++ Medium/Palindrome/Palindrome.java
@@ -0,0 +1,8 @@
+//A palindrome is a word, number, phrase, or other sequence of characters which reads the same backward as forward,
+
+public class Palindrome {
+    public static void main (String args[]){
+        
+
+    }
+}
