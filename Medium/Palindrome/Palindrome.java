0657141ba9f35134b2585ca5e63b82067ccefb5d
Udara Vimukthi <uvlakshan@gmail.com>
2021-08-25 19:41:25 +0530

    Added the codes to check the number

--- Medium/Palindrome/Palindrome.java
+++ Medium/Palindrome/Palindrome.java
@@ -2,7 +2,15 @@
 
 public class Palindrome {
     public static void main (String args[]){
-        
+        int r,sum=0,temp;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number to check :")
+        int n = scan.nextInt();
+
+        temp = n;
+        while(n>0){
+            r= n%10; // getting remainder
+        }
 
     }
 }
