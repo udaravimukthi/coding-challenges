4244e56bfd7cfdf0153e6f5a3d513d279d9d9864
Udara Vimukthi <uvlakshan@gmail.com>
2021-08-25 20:06:12 +0530

    Added completed code to check the number whether palindrome or not

--- Medium/Palindrome/Palindrome.java
+++ Medium/Palindrome/Palindrome.java
@@ -1,10 +1,10 @@
 //A palindrome is a word, number, phrase, or other sequence of characters which reads the same backward as forward,
-
+import java.util.Scanner;
 public class Palindrome {
     public static void main (String args[]){
         int r,sum=0,temp;
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter a number to check :")
+        System.out.println("Enter a number to check :");
         int n = scan.nextInt();
 
         temp = n;
