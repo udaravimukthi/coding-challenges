3e4b568243fc50ffeba1ea59614deb518a1897ad
Udara Vimukthi <uvlakshan@gmail.com>
2021-08-25 19:46:02 +0530

    Added change

--- Medium/Palindrome/Palindrome.java
+++ Medium/Palindrome/Palindrome.java
@@ -10,7 +10,16 @@ public class Palindrome {
         temp = n;
         while(n>0){
             r= n%10; // getting remainder
+            sum = (sum*10) + r;
+            n = n/10;
         }
+
+        if(temp==sum){
+            System.out.println("Palindrome number");
+        }else{
+            System.out.println("not palindrome");
+        }
+        
 
     }
 }
