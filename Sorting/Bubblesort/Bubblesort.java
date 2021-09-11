8b71403574a4e262b2cd04424fd4fa9bbbd1fb40
Udara Vimukthi <uvlakshan@gmail.com>
2021-09-12 01:59:30 +0530

    Added bubblesort java file

---
+++ Sorting/Bubblesort/Bubblesort.java
@@ -0,0 +1,31 @@
+public class Bubblesort {
+    public static void main(String args[]){
+        Bubblesort ob = new Bubblesort();
+        int arr[] = {64,34,25,12,22,11,90};
+        ob.Bubblesort(arr);
+        ob.printArray(arr);
+    }
+
+    void Bubblesort(int arr[]){
+        int n = arr.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]> arr[j+1]){
+                    //swap arr[j+1] and arr[j]
+                   int temp =arr[j];
+                   arr[j] = arr[j+1];
+                   arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    void printArray(int arr[]){
+        int n= arr.length;
+        for(int i=0;i<n; ++i){
+            System.out.print(arr[i] + " ");
+        //System.out.println();
+        }
+
+    }
+}
