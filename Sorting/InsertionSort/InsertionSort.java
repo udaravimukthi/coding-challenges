16b966446b9385989df96928dc8253bc5de0a9a3
Udara Vimukthi <uvlakshan@gmail.com>
2021-09-13 23:45:01 +0530

    Added change in the insertion sort

--- Sorting/InsertionSort/InsertionSort.java
+++ Sorting/InsertionSort/InsertionSort.java
@@ -7,12 +7,17 @@ public class InsertionSort {
     }
 
     void sort(int arr[]){
-        int n = arrr.length;
-        for(int i; i<n; ++i){
+        int n = arr.length;
+        for(int i=1; i<n; ++i){
             int key = arr[i];
             int j = i-1;
 
-            while 
+            while (j>=0 && arr[j]>key){
+                arr[j+1] = arr[j];
+                j=j-1;
+            }
+            arr[j+1] = key;
+
         }
     }
 }
