3e224195153202d4b708cd9f28ebd0f5bef8899f
Udara Vimukthi <uvlakshan@gmail.com>
2021-09-13 23:32:08 +0530

    Added new sort insertion sort

---
+++ Sorting/InsertionSort/InsertionSort.java
@@ -0,0 +1,18 @@
+public class InsertionSort {
+    public static void main (String args[]){
+        int arr[] = {12,11,13,5,6};
+        InsertionSort ob = new InsertionSort();
+        ob.sort(arr);
+        printArray(arr);
+    }
+
+    void sort(int arr[]){
+        int n = arrr.length;
+        for(int i; i<n; ++i){
+            int key = arr[i];
+            int j = i-1;
+
+            while 
+        }
+    }
+}
