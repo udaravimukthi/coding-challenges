60bfd8db6cbcb9563a02486d244b5807519a152d
Udara Vimukthi <uvlakshan@gmail.com>
2021-09-14 00:09:47 +0530

    finished the insertion sort

--- Sorting/InsertionSort/InsertionSort.java
+++ Sorting/InsertionSort/InsertionSort.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
+
 public class InsertionSort {
     public static void main (String args[]){
-        int arr[] = {12,11,13,5,6};
+        Scanner sc = new Scanner(System.in);
+        int [] numbers = new int[5];
+        for (int i = 0; i < numbers.length; i++)
+        {
+        System.out.println("Please enter number");
+        numbers[i] = sc.nextInt();
+        }
+
+        //int arr[] = {12,11,13,5,6};
         InsertionSort ob = new InsertionSort();
-        ob.sort(arr);
-        printArray(arr);
+        ob.sort(numbers);
+        printArray(numbers);
     }
 
     void sort(int arr[]){
