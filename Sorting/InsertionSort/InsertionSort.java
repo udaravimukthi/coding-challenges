da807bd2b7b20d4a7c92289f4d964db44330573b
Udara Vimukthi <uvlakshan@gmail.com>
2021-09-13 23:55:45 +0530

    Added change

--- Sorting/InsertionSort/InsertionSort.java
+++ Sorting/InsertionSort/InsertionSort.java
@@ -25,7 +25,7 @@ public class InsertionSort {
         int n = arr.length;
         for (int i=0; i<n ; ++i){
             System.out.print(arr[i] + " ");
-        System.out.println();
+        //System.out.println();
         }
     }
 }
