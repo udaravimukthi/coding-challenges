7a96c1e632a0f6b96c001515708b5008548f2386
Udara Vimukthi <uvlakshan@gmail.com>
2021-09-16 22:40:00 +0530

    added change

--- Sorting/QuickSort/QuickSort.java
+++ Sorting/QuickSort/QuickSort.java
@@ -21,4 +21,17 @@ public class QuickSort {
             quickSort(arr, pi+1, high);
         }
     }
+
+    static int partition(int[] arr, int low, int high){
+        int pivot = arr[high];
+        int i = (low-1);
+
+        for (int j = low; j<=high-1; j++){
+            if(arr[j]<pivot){
+                i++;
+                swap(arr,i,j);
+            }
+        }
+        swap(arr)
+    }
 }
