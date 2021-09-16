6c44b55d42c88668125ef9bf668491af35b7d186
Udara Vimukthi <uvlakshan@gmail.com>
2021-09-16 22:35:04 +0530

    Addded partition part to the code

--- Sorting/QuickSort/QuickSort.java
+++ Sorting/QuickSort/QuickSort.java
@@ -13,4 +13,12 @@ public class QuickSort {
             System.out.print(arr[i] + "");
         }
     }
+
+    static void quickSort(int[] arr, int low, int high){
+        if(low<high){
+            int pi = partition(arr, low, high);
+            quickSort(arr,low, pi-1);
+            quickSort(arr, pi+1, high);
+        }
+    }
 }
