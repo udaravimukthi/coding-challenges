6ee2539b38759e5f93fe99d10e54f98b57698552
Udara Vimukthi <uvlakshan@gmail.com>
2021-09-16 22:49:10 +0530

    added changes in quick sort java file

--- Sorting/QuickSort/QuickSort.java
+++ Sorting/QuickSort/QuickSort.java
@@ -10,7 +10,7 @@ public class QuickSort {
 
     static void printArray(int[] arr, int size){
         for(int i=0; i<size; i++){
-            System.out.print(arr[i] + "");
+            System.out.print(arr[i] + " ");
         }
     }
 
