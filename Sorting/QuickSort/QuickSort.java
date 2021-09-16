64a1455e00b04866e445eb01a6787615cb01d2f7
Udara Vimukthi <uvlakshan@gmail.com>
2021-09-16 22:26:11 +0530

    Added quick sort part

---
+++ Sorting/QuickSort/QuickSort.java
@@ -0,0 +1,16 @@
+public class QuickSort {
+    public static void main(String args[]){
+        int [] arr = { 10,7,8,9,1,5};
+        int n = arr.length;
+
+        quickSort(arr,0,n-1);
+        System.out.println("Sorted array:");
+        printArray(arr,n);
+    }
+
+    static void printArray(int[] arr, int size){
+        for(int i=0; i<size; i++){
+            System.out.print(arr[i] + "");
+        }
+    }
+}
