ac5fdfd484b20dbf57c0e3396d00c3b13575d19b
Udara Vimukthi <uvlakshan@gmail.com>
2021-09-17 23:42:10 +0530

    Added constructors

--- Sorting/BinaryTree/BinaryTree.java
+++ Sorting/BinaryTree/BinaryTree.java
@@ -1,3 +1,13 @@
 public class BinaryTree {
     //root of Binary tree
+    NOde root;
+
+    //constructors
+    BinaryTree(int key){
+        root = new Node(key);
+    }
+
+    BinaryTree(){
+        root = null;
+    }
 }
