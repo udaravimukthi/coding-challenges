747b02d61c01a6c2cb25f2b347eba873493a4332
Udara Vimukthi <uvlakshan@gmail.com>
2021-09-17 23:47:40 +0530

    Added changes in the binary tree

--- Sorting/BinaryTree/BinaryTree.java
+++ Sorting/BinaryTree/BinaryTree.java
@@ -15,7 +15,10 @@ public class BinaryTree {
         BinaryTree tree = new BinaryTree();
         
         //create root
-        
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
 
     }
 }
