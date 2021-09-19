569fcc7a75665d3e903bfac74a875bdab105fc50
Udara Vimukthi <uvlakshan@gmail.com>
2021-09-20 00:06:14 +0530

    Added change in file

--- Sorting/BinarySearchTree/BinarySearchTree.java
+++ Sorting/BinarySearchTree/BinarySearchTree.java
@@ -19,7 +19,14 @@ public class BinarySearchTree {
 
     public static void main(String args[]){
         BinarySearchTree tree = new BinarySearchTree();
-
+        
+        /* Let us create following BST
+              50
+           /     \
+          30      70
+         /  \    /  \
+       20   40  60   80 */
+        
         tree.insert(50);
         tree.insert(30);
         tree.insert(20);
@@ -30,5 +37,37 @@ public class BinarySearchTree {
         
         //print inorder traversal of the BST
         tree.inorder();
+    }
+
+    void insert(int key){
+        root = insertRec(root,key);
+    }
+
+    Node insertRec(Node root, int key){
+        if(root == null){
+            root = new Node(key);
+            return root;
+        }
+        if (key<root.key){
+            root.left = insertRec(root.left, key);
+        }else if(key>root.key){
+            root.right = insertRec(root.right, key);
+        }
+
+        return root;
+        
+    }
+
+    void inorder(){
+        inorderRec(root);
+    }
+
+    void inorderRec(Node root)
+    {
+        if (root != null) {
+            inorderRec(root.left);
+            System.out.println(root.key);
+            inorderRec(root.right);
+        }
     }
 }
