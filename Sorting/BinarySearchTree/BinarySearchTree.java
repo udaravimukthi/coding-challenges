e927408c624dfd139e7be86edf7742668fa39472
Udara Vimukthi <uvlakshan@gmail.com>
2021-09-19 23:48:20 +0530

    Added changes in the code

--- Sorting/BinarySearchTree/BinarySearchTree.java
+++ Sorting/BinarySearchTree/BinarySearchTree.java
@@ -1,5 +1,11 @@
 public class BinarySearchTree {
     class Node{
-        
+        int key;
+        Node left,right;
+
+        public Node(int item){
+            key = item;
+            left = right =null;
+        }
     }
 }
