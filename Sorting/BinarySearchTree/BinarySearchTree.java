821e7be950900ebba0a8273973b1460a11e23b5f
Udara Vimukthi <uvlakshan@gmail.com>
2021-09-19 23:54:40 +0530

    Added some changes

--- Sorting/BinarySearchTree/BinarySearchTree.java
+++ Sorting/BinarySearchTree/BinarySearchTree.java
@@ -21,6 +21,11 @@ public class BinarySearchTree {
         BinarySearchTree tree = new BinarySearchTree();
 
         tree.insert(50);
-        tree
+        tree.insert(30);
+        tree.insert(20);
+        tree.insert(40);
+        tree.insert(70);
+        tree.insert(60);
+        tree.insert(80);
     }
 }
