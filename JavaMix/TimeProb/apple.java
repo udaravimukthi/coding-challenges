f37773866f50be1178175877ffc7b1ec1990f813
Udara Vimukthi <uvlakshan@gmail.com>
2021-09-21 23:49:42 +0530

    Added change

--- JavaMix/TimeProb/apple.java
+++ JavaMix/TimeProb/apple.java
@@ -5,6 +5,6 @@ public class apple {
         orobj.setTime(14,25,8); 
         System.out.println(orobj.standTime());
         System.out.println("Regular Time according to user input");
-        System.out.println(orobj.RegularTime());
+        System.out.println(orobj.RegularTime()); 
     }
 }
