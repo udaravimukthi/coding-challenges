4aefeb577b760418c3ffb252678aa1a09fdc6918
Udara Vimukthi <uvlakshan@gmail.com>
2021-09-26 23:30:25 +0530

    Added change in the code

--- JavaMix/ArrayReverse/ArrayReverse.java
+++ JavaMix/ArrayReverse/ArrayReverse.java
@@ -1,21 +1,22 @@
 public class ArrayReverse {
     public static void main(String args[]) {
         int[] arr = { 2, 5, 7, 9, 8 };
-        ArrayReverse rev = new ArrayReverse();
-        System.out.println("Reversed Array");
-        rev.reverse(arr);
-        rev.printArray(arr);
+        reverse(arr, arr.length);
 
     }
 
-    void reverse(int arr[]){
-        int n = arr.length;
+    static void reverse(int arr[], int n){
+
+        int[] b = new int[n];
+        int j = n ;
         for(int i=0; i<n ; i++){
-            arr
+            b[j-1] = arr[i];
+            j = j-1;
         }
-    }
 
-    void printArray(int arr[]) {
-
+        System.out.println("Reversed array is :");
+        for (int k =0 ; k<n; k++){
+            System.out.println(b[k])
+        }
     }
 }
