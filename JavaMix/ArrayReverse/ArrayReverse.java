edbdbff3e4659dc20a620c819e48501eaae446b2
Udara Vimukthi <uvlakshan@gmail.com>
2021-09-26 23:38:59 +0530

    Completed arrray reverse java code

--- JavaMix/ArrayReverse/ArrayReverse.java
+++ JavaMix/ArrayReverse/ArrayReverse.java
@@ -5,18 +5,18 @@ public class ArrayReverse {
 
     }
 
-    static void reverse(int arr[], int n){
+    static void reverse(int arr[], int n) {
 
         int[] b = new int[n];
-        int j = n ;
-        for(int i=0; i<n ; i++){
-            b[j-1] = arr[i];
-            j = j-1;
+        int j = n;
+        for (int i = 0; i < n; i++) {
+            b[j - 1] = arr[i];
+            j = j - 1;
         }
 
         System.out.println("Reversed array is :");
-        for (int k =0 ; k<n; k++){
-            System.out.println(b[k])
+        for (int k = 0; k < n; k++) {
+            System.out.println(b[k]);
         }
     }
 }
