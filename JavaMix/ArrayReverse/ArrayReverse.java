799582d1175f7127ea4288926113399737ac5c4e
Udara Vimukthi <uvlakshan@gmail.com>
2021-09-26 23:11:41 +0530

    Added essensial code parts

--- JavaMix/ArrayReverse/ArrayReverse.java
+++ JavaMix/ArrayReverse/ArrayReverse.java
@@ -1,3 +1,21 @@
 public class ArrayReverse {
+    public static void main(String args[]) {
+        int[] arr = { 2, 5, 7, 9, 8 };
+        ArrayReverse rev = new ArrayReverse();
+        System.out.println("Reversed Array");
+        rev.reverse(arr);
+        rev.printArray(arr);
 
+    }
+
+    void reverse(int arr[]){
+        int n = arr.length;
+        for(int i=0; i<n ; i++){
+            arr
+        }
+    }
+
+    void printArray(int arr[]) {
+
+    }
 }
