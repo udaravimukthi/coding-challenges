86b2c26c00e0ea12512ab245b6bba0a2827bc246
Udara Vimukthi <uvlakshan@gmail.com>
2021-09-27 17:17:28 +0530

    Added duplicate fuction part

--- JavaMix/DupInArray/DupInArray.java
+++ JavaMix/DupInArray/DupInArray.java
@@ -11,7 +11,9 @@ public class DupInArray {
     static void duplicate(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
-
+                if (arr[i] == arr[j]) {
+                    System.out.println(arr[j]);
+                }
             }
         }
     }
