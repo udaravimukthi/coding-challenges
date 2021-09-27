a1f8ed7e5ea56810218760a1ffa660cafaf30eeb
Udara Vimukthi <uvlakshan@gmail.com>
2021-09-27 17:15:34 +0530

    Added the way how find the duplicated inthe array

---
+++ JavaMix/DupInArray/DupInArray.java
@@ -0,0 +1,18 @@
+package JavaMix.DupInArray;
+
+public class DupInArray {
+    public static void main(String args[]) {
+        int arr[] = { 2, 3, 4, 4, 3, 6, 7 };
+
+        System.out.print("Print duplicated numbers in array");
+        duplicate(arr);
+    }
+
+    static void duplicate(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+
+            }
+        }
+    }
+}
