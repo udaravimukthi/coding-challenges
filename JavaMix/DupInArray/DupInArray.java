4b1a4f912387943ecf32da22efc0bbd9a7bf73f7
Udara Vimukthi <uvlakshan@gmail.com>
2021-09-27 17:26:16 +0530

    Added change in the array

--- JavaMix/DupInArray/DupInArray.java
+++ JavaMix/DupInArray/DupInArray.java
@@ -1,6 +1,6 @@
 public class DupInArray {
     public static void main(String args[]) {
-        int arr[] = { 2, 3, 4, 4, 3, 6, 7, 8, 8, 8 };
+        int arr[] = { 2, 3, 4, 4, 3, 6, 7, 8, 8, 9 };
 
         System.out.print("Print duplicated numbers in array: \n");
         duplicate(arr);
