f900f832f1cddd10ae2307b54ff098e6e897fa9e
Udara Vimukthi <uvlakshan@gmail.com>
2021-09-28 16:13:45 +0530

    Added linked list class file also

--- JavaMix/LinkedList/LinkedListDemo.java
+++ JavaMix/LinkedList/LinkedListDemo.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class LinkedList {
+public class LinkedListDemo {
     public static void main(String args[]) {
         LinkedList<String> ll = new LinkedList<String>();
 
@@ -17,6 +17,8 @@ public class LinkedList {
         ll.remove(3);
         ll.removeFirst();
         ll.removeLast();
+
+        System.out.println(ll);
     }
 
 }
