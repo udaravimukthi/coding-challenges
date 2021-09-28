d190588164718d73aa65ef6954b3587fc9913906
Udara Vimukthi <uvlakshan@gmail.com>
2021-09-28 15:32:41 +0530

    Added elemets to the linked list

--- JavaMix/LinkedList/LinkedList.java
+++ JavaMix/LinkedList/LinkedList.java
@@ -4,5 +4,11 @@ public class LinkedList {
     public static void main(String args[]) {
         LinkedList<String> ll = new LinkedList<String>();
 
+        // Adding elements to the linked list
+        ll.add("A");
+        ll.add("B");
+        ll.addLast("C");
+        ll.addFirst("D");
+        ll.add(2, "E");
     }
 }
