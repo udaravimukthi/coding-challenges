bdb6fc1934792f7d214cd95b5be922fbe1901e35
Udara Vimukthi <uvlakshan@gmail.com>
2021-10-05 02:26:05 +0530

    Added the second way of thread creating using runnable interface

--- JavaMix2/Javathreads/Javathreads.java
+++ JavaMix2/Javathreads/Javathreads.java
@@ -15,5 +15,18 @@ class Multi extends Thread {
         Multi t1 = new Multi();
         t1.start(); // start the theread
     }
+}
 
+// By using implementing runnable interface
+class Multi2 implements Runnable {
+    public void run() {
+        System.out.println("Thread is running");
+    }
+
+    public static void main(String args[]) {
+        Multi2 m1 = new Multi2();
+        Thread t1 = new Thread(m1);
+
+        t1.start(); // creating a thread, here also output should be Thread is running
+    }
 }
