09ea0b749d183f041d4496485d3b068a9c823c53
Udara Vimukthi <uvlakshan@gmail.com>
2021-10-05 01:51:05 +0530

    by using extending the thread class way used

--- JavaMix2/Javathreads/Javathreads.java
+++ JavaMix2/Javathreads/Javathreads.java
@@ -1,3 +1,5 @@
+import jdk.javadoc.internal.doclets.formats.html.resources.standard;
+
 //java threads
 
 //1. By using , extending the java thread class
@@ -7,6 +9,11 @@
 class Multi extends Thread {
     public void run() {
         System.out.println("Thread is running");
+    }
+
+    public static void main(String args[]) {
+        Multi t1 = new Multi();
+        t1.start(); // start the theread
     }
 
 }
