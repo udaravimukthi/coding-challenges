990c8cfef1ef2effab681985ddd81212cc3963c8
Udara Vimukthi <uvlakshan@gmail.com>
2021-10-05 01:49:15 +0530

    Added java threading

---
+++ JavaMix2/Javathreads/Javathreads.java
@@ -0,0 +1,12 @@
+//java threads
+
+//1. By using , extending the java thread class
+//2. By using , by implementing runnable interface
+
+//By using extending the java thread class
+class Multi extends Thread {
+    public void run() {
+        System.out.println("Thread is running");
+    }
+
+}
