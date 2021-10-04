532535ca00e750ad2c986eb0fb6812e7070e9594
Udara Vimukthi <uvlakshan@gmail.com>
2021-10-05 02:33:17 +0530

    Added sleep method in java in threads

---
+++ JavaMix2/SleepMethod/SleepMethod.java
@@ -0,0 +1,17 @@
+// sleep method is pass running state to non runnable(blocking state)
+
+class TestSleepMethod extends Thread {
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            Thread.sleep(500); /// 500 miliseconds
+        }
+    }
+
+    public static void main(String args[]) {
+        TestSleepMethod t1 = new TestSleepMethod();
+        TestSleepMethod t2 = new TestSleepMethod();
+
+        t1.start();
+        t2.start(); // output should be 1 1 2 2 3 3 4 4 5 5
+    }
+}
