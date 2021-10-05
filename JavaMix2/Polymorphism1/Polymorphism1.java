7cddf0053ce867dda361c395c578d89d491bc69d
Udara Vimukthi <uvlakshan@gmail.com>
2021-10-06 02:26:42 +0530

    Added code in runtime polymorphism

--- JavaMix2/Polymorphism1/Polymorphism1.java
+++ JavaMix2/Polymorphism1/Polymorphism1.java
@@ -1,3 +1,26 @@
-public class Polymorphism1 {
+//runtime polymorphism
 
+public class Polymorphism1 {
+    public static void main(String[] args) {
+        Parent p1 = new Parent();
+        Child c1 = new Child();
+
+        System.out.println("Parent");
+        p1.foo();
+        System.out.println("Child");
+        c1.foo();
+    }
+}
+
+class Parent {
+    public void foo() {
+        System.out.println("fROM PARENT CLASS");
+    }
+}
+
+class Child extends Parent {
+    @Override
+    public void foo() {
+        System.out.println("fROM child class");
+    }
 }
